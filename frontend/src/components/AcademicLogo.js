5cf3a053411030e3842037602707ea1418d7d3d4
Neo Agent <neo@emergent.com>
2026-02-14 09:14:10 +0000

    auto-commit for b724a459-064a-4362-b592-25f19ab910d7

--- frontend/src/components/AcademicLogo.js
+++ frontend/src/components/AcademicLogo.js
@@ -93,7 +93,7 @@ const AcademicLogo = ({ size = 'large' }) => {
             letterSpacing: '0.05em'
           }}
         >
-          Educational Reforms Bureau
+          Education Reforms
         </h1>
         
         {/* Divider line */}
